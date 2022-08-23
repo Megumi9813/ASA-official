d899949b654f4999148788912693475050f92f5c
MegumiIgarashi <i.imeguumi@gmail.com>
2022-08-23 12:57:45 -0700

    added to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
+import React, {useEffect, useState}from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import './App.scss'
+import About from './pages/About'
+// import Footer from './components/Footer'
+import useContentful from './useContentful'
+import Programs from './pages/Programs'
+// import "slick-carousel/slick/slick.css"; 
+// import "slick-carousel/slick/slick-theme.css";
+import Book from './pages/Book'
+import ScrollToTop from './ScrollToTop'
+import ProgramIntro from './pages/ProgramIntro'
+import Blogs from './pages/Blogs'
+
+
 function App() {
+  ScrollToTop();
+  const [teachers, setTeachers] = useState([]);
+  const [blogs, setBlogs] = useState([]);
+  const [testimonials, setTestimonials] = useState([]);
+  const {getData} = useContentful();
+
+  useEffect(() => {
+    getData().then((res) => {
+      setTeachers(res.teacherSanitizedResponse);
+      setBlogs(res.blogSanitizedResponse);
+      setTestimonials(res.testimonialSanitizedResponse);
+    });
+  });
+
   return (
-    <div className="App">
-    </div>
-  );
+          <div className="App">
+            <Routes>
+              <Route path='/' element={<Home teachers={teachers} />} />
+              <Route path='/About' element={<About teachers={teachers} testimonials={testimonials} />} />
+              <Route path='/Programs' element={<Programs testimonials={testimonials} />} />
+              <Route path='/Book' element={<Book />}  />
+              <Route path='/Blog' element={<Blogs blogs={blogs} />}  />
+              <Route path='/ProgramIntro' element={<ProgramIntro />}  />
+            </Routes>
+            <Footer />
+          </div>
+        )
 }
 
-export default App;
+export default App
