f2c2c391c3413c21577cafeadd6a235cf357104c
MegumiIgarashi <i.imeguumi@gmail.com>
2022-09-02 11:23:47 -0700

    membership button in Programs

--- src/components/MembershipIntro.jsx
+++ src/components/MembershipIntro.jsx
@@ -3,29 +3,37 @@ import { Link } from 'react-router-dom'
 
 function MembershipIntro() {
   return (
-      <section id="membershipIntro">
-          <div className="programsMembership_container">
-            <div className="section_intro">
-                <div className="section_title font02 color02">Not Ready to take a 1:1 classes?</div>
-                <p>Subscribe our membership plan to make your 1:1 classes more effective!</p>
-            </div>
-            <div className="programsMembership">
-                <ul className="programsMembership_list">
-                    <li className="programsMembership_item">
-                        <img src="" alt="" />
-                        <div className="programsMembership_info">
-                            {/* <h5 className='programsMembership_title'></h5> */}
-                            <p></p>
-                        </div>
-                    </li>
-                </ul>
-                <Link to='#'>
-                    <button className='btn'>Membership Plan</button>
-                </Link>
-            </div>
+    <section id="membershipIntro">
+      <div className="programsMembership_container">
+        <div className="section_intro">
+          <div className="section_title font02 color02">
+            Not Ready to take a 1:1 classes?
+          </div>
+          <p>
+            Subscribe our membership plan to make your 1:1 classes more
+            effective!
+          </p>
         </div>
-      </section>
-  )
+        <div className="programsMembership">
+          <ul className="programsMembership_list">
+            <li className="programsMembership_item">
+              <img src="" alt="" />
+              <div className="programsMembership_info">
+                {/* <h5 className='programsMembership_title'></h5> */}
+                <p></p>
+              </div>
+            </li>
+          </ul>
+          <Link to="/asa-membership" target="_blank" rel="noopener noreferrer">
+            <button className="btn font01">Membership plan</button>
+          </Link>
+          {/* <Link to="#">
+            <button className="btn">Membership Plan</button>
+          </Link> */}
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default MembershipIntro
