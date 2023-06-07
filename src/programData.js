2cc3564bfd9220edc0aa75685f2227f75111b5b2
MegumiIgarashi <60909645+Megumi9813@users.noreply.github.com>
2023-06-07 11:51:43 +0900

    changed plan pics

--- src/programData.js
+++ src/programData.js
@@ -1,55 +1,55 @@
-import MACP from './assets/MACP.jpg';
+import MACP1 from './assets/MACP1.jpg';
 import TPP from './assets/TPP.jpg';
 import FA from './assets/F&A.jpg';
 import EPIC from './assets/EPIC.jpg';
 import BCP from './assets/BCP.jpg';
-import Group from './assets/Group.jpg';
+import Group1 from './assets/Group1.jpg';
 import Membership from './assets/Membership.jpg';
 
 export  const programData = [
-    {
-        id: 1,
-        title: "Personalized 1:1 Class",
-        url: MACP,
-    },
-    {
-        id: 2,
-        title: "Group Class",
-        url: Group,
-    },
-    {
-        id: 3,
-        title: "Membership Plan",
-        url: Membership,
-    },
-    {
-        id: 4,
-        title: "Mentorship and Academic Coaching Program (MACP)",
-        url: MACP,
-        para: "Gaining admission to elite academic institutions requires years of preparation. In addition to outstanding academic performance on standardized tests such as IELTS and SAT, candidates need to have an academic profile that demonstrates their passion for knowledge and a sense of purpose. With decades of experience in academic coaching, our mentorship program can help middle and high school, as well as post-secondary students excel in their conventional programs while developing the inquisitive disposition that universities seek."
-    },
-    {
-        id: 5,
-        title: "Test Preparation Program (TPP)",
-        url: TPP,
-        para: "Have a result in mind? Take the free diagnostic test and we will create a comprehensive study plan to help you achieve your desired result on the SAT, GMAT, IELTS, CELPIP, TOEFL or other standardized exams."
-    },
-    {
-        id: 6,
-        title: "Business Communication Program (BCP)",
-        url: BCP,
-        para: "Whether you are a new immigrant or a working professional, strong English skills are crucial to career advancement in any industry. From landing that new job, to becoming a valuable team player, to securing that big promotion, effective communication in a professional setting is the foundation of how you will interact with coworkers and managers and contribute within your workplace."
-    },
-    {
-        id: 7,
-        title: "Fluency and Accuracy Program (F&A)",
-        url: FA,
-        para: "This mixed skill class is ideal for students who simply want to become more comfortable and confident speaking English. This course provides students with a wide variety of ways to engage with the language using activities ranging from explicit grammar and vocabulary instruction to spontaneous interaction and conversation. Learners receive direct feedback on their speaking and writing as well as multiple opportunities for practice and skill-building."
-    },
-    {
-        id: 8,
-        title: "English Program for Immigrants in Canada (EPIC)",
-        url: EPIC,
-        para: "A strong grasp of English helps new Canadian immigrants navigate the world around them, making everything from accessing resources to meeting new friends much less intimidating. Many who don’t speak English fluently face a number of obstacles in Canada such as preparing for career changes, learning about Canadian college or simply trying to keep up with their children’s progress in English. Here at ASA, we offer the English for English Program for Immigrants in Canada (EPIC) that is tailor-made for new immigrants who may be considering career changes or simply looking to improve their English as they adapt to the Canadian lifestyle. While many standard ESL programs assume a foundation in English abilities, EPIC recognizes that not all English students have an expansive background in formal English education. We focus on building foundational and intermediate skills by providing students with functional tasks and skills to address weaknesses in their use of everyday English."
-    },
+  {
+    id: 1,
+    title: "Personalized 1:1 Class",
+    url: MACP1,
+  },
+  {
+    id: 2,
+    title: "Group Class",
+    url: Group1,
+  },
+  {
+    id: 3,
+    title: "Membership Plan",
+    url: Membership,
+  },
+  {
+    id: 4,
+    title: "Mentorship and Academic Coaching Program (MACP)",
+    url: MACP1,
+    para: "Gaining admission to elite academic institutions requires years of preparation. In addition to outstanding academic performance on standardized tests such as IELTS and SAT, candidates need to have an academic profile that demonstrates their passion for knowledge and a sense of purpose. With decades of experience in academic coaching, our mentorship program can help middle and high school, as well as post-secondary students excel in their conventional programs while developing the inquisitive disposition that universities seek.",
+  },
+  {
+    id: 5,
+    title: "Test Preparation Program (TPP)",
+    url: TPP,
+    para: "Have a result in mind? Take the free diagnostic test and we will create a comprehensive study plan to help you achieve your desired result on the SAT, GMAT, IELTS, CELPIP, TOEFL or other standardized exams.",
+  },
+  {
+    id: 6,
+    title: "Business Communication Program (BCP)",
+    url: BCP,
+    para: "Whether you are a new immigrant or a working professional, strong English skills are crucial to career advancement in any industry. From landing that new job, to becoming a valuable team player, to securing that big promotion, effective communication in a professional setting is the foundation of how you will interact with coworkers and managers and contribute within your workplace.",
+  },
+  {
+    id: 7,
+    title: "Fluency and Accuracy Program (F&A)",
+    url: FA,
+    para: "This mixed skill class is ideal for students who simply want to become more comfortable and confident speaking English. This course provides students with a wide variety of ways to engage with the language using activities ranging from explicit grammar and vocabulary instruction to spontaneous interaction and conversation. Learners receive direct feedback on their speaking and writing as well as multiple opportunities for practice and skill-building.",
+  },
+  {
+    id: 8,
+    title: "English Program for Immigrants in Canada (EPIC)",
+    url: EPIC,
+    para: "A strong grasp of English helps new Canadian immigrants navigate the world around them, making everything from accessing resources to meeting new friends much less intimidating. Many who don’t speak English fluently face a number of obstacles in Canada such as preparing for career changes, learning about Canadian college or simply trying to keep up with their children’s progress in English. Here at ASA, we offer the English for English Program for Immigrants in Canada (EPIC) that is tailor-made for new immigrants who may be considering career changes or simply looking to improve their English as they adapt to the Canadian lifestyle. While many standard ESL programs assume a foundation in English abilities, EPIC recognizes that not all English students have an expansive background in formal English education. We focus on building foundational and intermediate skills by providing students with functional tasks and skills to address weaknesses in their use of everyday English.",
+  },
 ];
