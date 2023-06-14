ba09eee77b517a179277f91bdcae120dc7d9bc27
MegumiIgarashi <60909645+Megumi9813@users.noreply.github.com>
2023-06-13 20:06:55 -0700

    emailJS

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,54 +33,71 @@ function Contact() {
 
   return (
     <div id="contact">
-        <div className={contactOpen ? "contact_background open" : "contact_background"}></div>
-        <div className={contactOpen ? "contact_container open" : "contact_container"}>
-            <div className="contact-wrapper">
-                <div className="contact_header">
-                <button onClick={() => setContactOpen(false)}>
-                    <FontAwesomeIcon icon="fa-xmark" />
-                </button>
-                </div>
-                <div className="contact_body">
-                <div className="contact_half-img">
-                    <img src={ContactImg} alt="" />
-                </div>
-                <div className="contact_half-form">
-                    <h3>Let's have a chat!</h3>
-                    <form ref={form} onSubmit={sendEmail}>
-                    <div className="form__item">
-                        <label className="form_item-label">Name</label>
-                        <input className="input" name="user_name" type="text"></input>
-                    </div>
-                    <div className="form__item">
-                        <label className="form_item-label">Email</label>
-                        <input className="input" name="user_email" type="email"></input>
-                    </div>
-                    <div className="form__item">
-                        <label className="form_item-label">Inquiry</label>
-                        <textarea className="input" name="inquiry" type="text"></textarea>
-                    </div>
-                    <button id="contact_submit" className="form_submit">
-                        Submit!
-                    </button>
-                    </form>
-                    <div className="modal__overlay modal__overlay--loading">
-                        <FontAwesomeIcon icon="fa-spinner" />
-                    </div>
-                    <div className="modal__overlay modal__overlay--success font02">
-                        Thank you for the message! 
-                    </div>
-                </div>
-                </div>
-            </div>
-        </div>
-        <Link to="#">
-            <button className='mail_btn' onClick={() => setContactOpen(!contactOpen)}>
-                <FontAwesomeIcon icon="fa-envelope" />
+      <div
+        className={
+          contactOpen ? "contact_background open" : "contact_background"
+        }
+      ></div>
+      <div
+        className={contactOpen ? "contact_container open" : "contact_container"}
+      >
+        <div className="contact-wrapper">
+          <div className="contact_header">
+            <button onClick={() => setContactOpen(false)}>
+              <FontAwesomeIcon icon="fa-xmark" />
             </button>
-        </Link>
+          </div>
+          <div className="contact_body">
+            <div className="contact_half-img">
+              <img src={ContactImg} alt="" />
+            </div>
+            <div className="contact_half-form">
+              <h3>Let's have a chat!</h3>
+              <form ref={form} onSubmit={sendEmail}>
+                <div className="form__item">
+                  <label className="form_item-label">Name</label>
+                  <input className="input" name="user_name" type="text"></input>
+                </div>
+                <div className="form__item">
+                  <label className="form_item-label">Email</label>
+                  <input
+                    className="input"
+                    name="user_email"
+                    type="email"
+                  ></input>
+                </div>
+                <div className="form__item">
+                  <label className="form_item-label">Inquiry</label>
+                  <textarea
+                    className="input"
+                    name="message"
+                    type="text"
+                  ></textarea>
+                </div>
+                <button id="contact_submit" className="form_submit">
+                  Submit!
+                </button>
+              </form>
+              <div className="modal__overlay modal__overlay--loading">
+                <FontAwesomeIcon icon="fa-spinner" />
+              </div>
+              <div className="modal__overlay modal__overlay--success font02">
+                Thank you for the message!
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Link to="#">
+        <button
+          className="mail_btn"
+          onClick={() => setContactOpen(!contactOpen)}
+        >
+          <FontAwesomeIcon icon="fa-envelope" />
+        </button>
+      </Link>
     </div>
-  )
+  );
 }
 
 export default Contact
